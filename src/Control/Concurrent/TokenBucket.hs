061e0ae637d8f6708652ad88ad0fd343e815e74e
Aditya Manthramurthy <aditya.mmy@gmail.com>
2023-06-07 10:34:27 -0700

    Make all data types strict

--- src/Control/Concurrent/TokenBucket.hs
+++ src/Control/Concurrent/TokenBucket.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE StrictData #-}
 
 module Control.Concurrent.TokenBucket
   ( Rate (..),
--- src/Network/Wai/RateLimitMiddleware.hs
+++ src/Network/Wai/RateLimitMiddleware.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE StrictData #-}
+
 -- |
 -- Module     : Network.Wai.RateLimitMiddleware
 --
